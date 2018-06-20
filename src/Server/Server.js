eff059644df7fcf8e53dae641bf36890a08d52f3
sbowen88 <sbowey@gmail.com>
2018-06-19 23:12:09 -0600

    setting up sockets

--- src/Server/Server.js
+++ src/Server/Server.js
@@ -5,7 +5,19 @@ const io = require("socket.io").listen(server);
 users = [];
 connections = [];
 
+server.listen(process.env.PORT || 3000);
+console.log('server running')
 
-server.listen(process.env.PORT||3000)
+app.get("/", function(res, resp) {
+  res.sendFile(__dirname + "index/html");
+});
 
-app.get('/')
+
+io.sockets.on('connection', function(socket){
+    connections.push();
+    console.log('connected %s sockets connected', connections.length);
+
+    //Disconnect 
+    connections.splice(connections.indexOf(socket),1)
+    console.log('Disconnect: %s sockets connected', connections.length)
+})
