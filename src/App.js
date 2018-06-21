d54679013e6911fbae4c257cd566a5c0aeb2cb61
sbowen88 <sbowey@gmail.com>
2018-06-20 23:51:24 -0600

    styling changes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,28 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header>
-          
-        </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="well">
+              <h3>Online Users</h3>
+              <ul className="list-group" id="users" />
+            </div>
+          </div>
+          <div className="col-md-8">
+            <div className="chat" id="chat" />
+            <form action="" id="message-form">
+              <label>Enter Message</label>
+              <textarea className="form-control" id="message" />
+              <br />
+              <input
+                type="submit"
+                className="btn btn-primary"
+                value="Send Message"
+              />
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
